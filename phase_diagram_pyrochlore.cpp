56f89ad8aab03d3307fd271c54138e287330e1a5
zhengbang <zhouzb79@gmail.com>
2024-12-22 04:27:16 -0500

    minor tweak

--- phase_diagram_pyrochlore.cpp
+++ phase_diagram_pyrochlore.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char** argv) {
     double k_B = 0.08620689655;
     double mu_B = 5.7883818012e-2;
-    phase_diagram_pyrochlore_0_field(70, "MC_phase_diagram_pyrochore");
+    int resolution = argv[1] ? atoi(argv[1]) : 0;
+    string dir = argv[2] ? argv[2] : "";
+    phase_diagram_pyrochlore_0_field(resolution, dir);
     return 0;
 }
