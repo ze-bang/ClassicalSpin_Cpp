204044fd184d7a4a23c6687e3accc2c763886c04
ze-bang <zhouzb79@gmail.com>
2025-01-08 20:50:20 -0500

    pc merge

--- phase_diagram_pyrochlore.cpp
+++ phase_diagram_pyrochlore.cpp
@@ -4,8 +4,25 @@
 int main(int argc, char** argv) {
     double k_B = 0.08620689655;
     double mu_B = 5.7883818012e-2;
-    int resolution = argv[1] ? atoi(argv[1]) : 0;
-    string dir = argv[2] ? argv[2] : "";
-    phase_diagram_pyrochlore_0_field(resolution, dir);
+    double Jpm_min = argv[1] ? atof(argv[1]) : 0.0;
+    double Jpm_max = argv[2] ? atof(argv[2]) : 0.0;
+    int num_Jpm = argv[3] ? atoi(argv[3]) : 0;
+    double Jpmpm = argv[4] ? atof(argv[4]) : 0.0;
+    double h_min = argv[5] ? atof(argv[5]) : 0.0;
+    double h_max = argv[6] ? atof(argv[6]) : 0.0;
+    int num_h = argv[7] ? atoi(argv[7]) : 0;
+    string dir_string = argv[8] ? argv[8] : "001";
+    array<double, 3> field_dir;
+    if (dir_string == "001"){
+        field_dir = {0,0,1};
+    }else if(dir_string == "110"){
+        field_dir = {1/sqrt(2), 1/sqrt(2), 0};
+    }else if(dir_string == "1-10"){
+        field_dir = {1/sqrt(2), -1/sqrt(2), 0};
+    }else{
+        field_dir = {1/sqrt(3),1/sqrt(3),1/sqrt(3)};
+    }
+    string dir = argv[9] ? argv[9] : "";
+    phase_diagram_pyrochlore(double Jpm_min, double Jpm_max, int num_Jpm, double h_min, double h_max, int num_h, double Jpmpm, array<double, 3> field_dir, string dir){
     return 0;
 }
