b7389b76e7a84d61fea287f7020604c288bf6ac2
Zhengbang Zhou <zhouzb79@cedar1.int.cedar.computecanada.ca>
2025-01-24 12:48:38 -0800

    archaic cedar merge

--- test.cpp
+++ test.cpp
@@ -525,7 +525,7 @@ void  simulated_annealing_pyrochlore(double Jxx, double Jyy, double Jzz, double 
     MC.simulated_annealing(5, 1e-4, 10000, 0, true, dir);
 }
 
-void parallel_tempering_pyrochlore(double T_start, double T_end, double Jxx, double Jyy, double Jzz, double gxx, double gyy, double gzz, double h, array<double, 3> field_dir, string dir, const vector<int> &rank_to_write){
+void parallel_tempering_pyrochlore(double T_start, double T_end, double Jxx, double Jyy, double Jzz, double gxx, double gyy, double gzz, double h, array<double, 3> field_dir, string dir, const vector<int> &rank_to_write, bool MD){
     filesystem::create_directory(dir);
     Pyrochlore<3> atoms;
 
@@ -583,6 +583,10 @@ void parallel_tempering_pyrochlore(double T_start, double T_end, double Jxx, dou
     if (!finalized){
         MPI_Finalize();
     }
+
+    if (MD){
+        MC.molecular_dynamics(temps[0], 1e-3, 10000, 0, 0, 1000, 1e-1, dir);
+    }
 }
 
 void phase_diagram_pyrochlore(double Jpm_min, double Jpm_max, int num_Jpm, double h_min, double h_max, int num_h, double Jpmpm, array<double, 3> field_dir, string dir){
@@ -625,48 +629,51 @@ int main(int argc, char** argv) {
     // MD_TmFeO3(1, -1.0, -0.06, "test_L=12");
     // MD_kitaev_honeycomb(1, -1.0, 0.25, -0.02, 0.7, "integrity_test");
     string dir = "test_long_MD=0.7/";
-    full_nonlinearspectroscopy_kitaev_honeycomb(1, 1, 1e-7, 0, -600, -0.25, -600, 600, 0.25, -1.0, 0.25, -0.02, 0.7, dir, true);
+    // full_nonlinearspectroscopy_kitaev_honeycomb(1, 1, 1e-7, 0, -600, -0.25, -600, 600, 0.25, -1.0, 0.25, -0.02, 0.7, dir, true);
 
     // MD_pyrochlore(1, 0.062/0.063, 1.0, 0.011/0.063, 0, 0, 1, 8, {1/sqrt(2),1/sqrt(2),0}, "CZO_h=4T");
 
     // MD_TmFeO3_Fe(1, 1000*k_B, 2*k_B, 4.625, 4.625, 4.625, 0.158, 0.158, 0.158, 0, -0.023, 0.0, 0.0, 0, {0,0,1}, "TmFeO3_Fe_Magnon_MD_real_meV");
     // simulated_annealing_honeycomb(1, 1e-6, -1, 0.25, -0.02, 0.7, "test_simulated_annealing");
-    // int initialized;
-    // MPI_Initialized(&initialized);
-    // if (!initialized){
-    //     MPI_Init(NULL, NULL);
-    // }
-    // int size;
-    // MPI_Comm_size(MPI_COMM_WORLD, &size);
-    // vector<int> rank_to_write = {size-1};
-    // // parallel_tempering_honeycomb(1, 1e-6, -1, 0.25, -0.02, 0.7, "test_parallel", rank_to_write);
-    // double Jpm_start = argv[1] ? atof(argv[1]) : 0.0;
-    // double Jpm_end = argv[2] ? atof(argv[2]) : 0.0;
-    // int num_Jpm = argv[3] ? atoi(argv[3]) : 0;
-    // double Jpmpm = argv[4] ? atof(argv[4]) : 0.0;
-    // double h_min = argv[5] ? atof(argv[5]) : 0.0;
-    // double h_max = argv[6] ? atof(argv[6]) : 0.0;
-    // double num_H = argv[7] ? atoi(argv[7]) : 0;
-    // string dir_string = argv[8] ? argv[8] : "001";
-    // array<double, 3> field_dir;
-    // if (dir_string == "001"){
-    //     field_dir = {0,0,1};
-    // }else if(dir_string == "110"){
-    //     field_dir = {1/sqrt(2), 1/sqrt(2), 0};
-    // }else{
-    //     field_dir = {1/sqrt(3),1/sqrt(3),1/sqrt(3)};
-    // }
-    // int SLURM_TASK_ID = argv[9] ? atoi(argv[9]) : 0;
-    // int Jpm_ind = SLURM_TASK_ID % num_Jpm;
-    // int h_ind = SLURM_TASK_ID / num_Jpm;
-    // double Jpm = Jpm_start + Jpm_ind*(Jpm_end-Jpm_start)/num_Jpm;
-    // double h = h_min + h_ind*(h_max-h_min)/num_H;
-    // string dir_name = argv[10] ? argv[10] : "";
-    // filesystem::create_directory(dir_name);
-    // string sub_dir = dir_name + "/Jpm_" + std::to_string(Jpm) + "_h_" + std::to_string(h) + "_" + std::to_string(Jpm_ind) + "_" + std::to_string(h_ind);
-    // int MPI_n_tasks = argv[11] ? atoi(argv[11]) : 1;
-    // std::cout << "Initializing parallel tempering calculation with parameters: " << "T = " << 10 << "-" << 1e-3 << " Jpm: " << Jpm << " Jpmpm: " << Jpmpm << " H: " << h << " field direction : " << dir_string << " saving to: " << dir_name << endl;
-    // parallel_tempering_pyrochlore(10, 1e-3, -2*Jpm - 2*Jpmpm, 1, -2*Jpm + 2*Jpmpm, 0, 0, 1, h, field_dir, sub_dir, {MPI_n_tasks-1});
+    int initialized;
+    MPI_Initialized(&initialized);
+    if (!initialized){
+        MPI_Init(NULL, NULL);
+    }
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    vector<int> rank_to_write = {size-1};
+    // parallel_tempering_honeycomb(1, 1e-6, -1, 0.25, -0.02, 0.7, "test_parallel", rank_to_write);
+    double Jpm_start = argv[1] ? atof(argv[1]) : 0.0;
+    double Jpm_end = argv[2] ? atof(argv[2]) : 0.0;
+    int num_Jpm = argv[3] ? atoi(argv[3]) : 0;
+    double Jpmpm = argv[4] ? atof(argv[4]) : 0.0;
+    double h_min = argv[5] ? atof(argv[5]) : 0.0;
+    double h_max = argv[6] ? atof(argv[6]) : 0.0;
+    double num_H = argv[7] ? atoi(argv[7]) : 0;
+    string dir_string = argv[8] ? argv[8] : "001";
+    array<double, 3> field_dir;
+    if (dir_string == "001"){
+        field_dir = {0,0,1};
+    }else if(dir_string == "110"){
+        field_dir = {1/sqrt(2), 1/sqrt(2), 0};
+    }else if(dir_string == "1-10"){
+        field_dir = {1/sqrt(2), -1/sqrt(2), 0};
+    }else{
+        field_dir = {1/sqrt(3),1/sqrt(3),1/sqrt(3)};
+    }
+    int SLURM_TASK_ID = argv[9] ? atoi(argv[9]) : 0;
+    int Jpm_ind = SLURM_TASK_ID % num_Jpm;
+    int h_ind = SLURM_TASK_ID / num_Jpm;
+    double Jpm = Jpm_start + Jpm_ind*(Jpm_end-Jpm_start)/num_Jpm;
+    double h = h_min + h_ind*(h_max-h_min)/num_H;
+    string dir_name = argv[10] ? argv[10] : "";
+    filesystem::create_directory(dir_name);
+    string sub_dir = dir_name + "/Jpm_" + std::to_string(Jpm) + "_h_" + std::to_string(h) + "_" + std::to_string(Jpm_ind) + "_" + std::to_string(h_ind);
+    int MPI_n_tasks = argv[11] ? atoi(argv[11]) : 1;
+    bool MD = argv[12] ? atoi(argv[12]) : 0;
+    std::cout << "Initializing parallel tempering calculation with parameters: " << "T = " << 1e-3 << "-" << 10 << " Jpm: " << Jpm << " Jpmpm: " << Jpmpm << " H: " << h << " field direction : " << dir_string << " saving to: " << dir_name << endl;
+    parallel_tempering_pyrochlore(1e-3, 10, -2*Jpm - 2*Jpmpm, 1, -2*Jpm + 2*Jpmpm, 0, 0, 1, h, field_dir, sub_dir, {0}, MD);
 
     // simulated_annealing_pyrochlore(-0.4, 1, 0.4, 0, 0, 1, 0, {0,0,1}, "test");
 
