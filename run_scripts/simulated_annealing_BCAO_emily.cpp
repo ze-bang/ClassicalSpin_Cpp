5316408f4d09ec188b59fc4e1ea51c27bac258ff
ze-bang <zhouzb79@gmail.com>
2025-09-21 11:55:58 -0400

    good

--- run_scripts/simulated_annealing_BCAO_emily.cpp
+++ run_scripts/simulated_annealing_BCAO_emily.cpp
@@ -237,7 +237,7 @@ void sim_BCAO_honeycomb(size_t num_trials, double h, array<double, 3> field_dir,
     for(size_t i = 0; i < num_trials; ++i){
         filesystem::create_directories(dir + "/" + std::to_string(i));
         lattice<3, 2, 24, 24, 1> MC(&atoms, 1, true);
-        auto SA_params = MC.tune_simulated_annealing(0.5, 20.0);
+        auto SA_params = MC.tune_simulated_annealing(0.5, 10.0, false, 20, 1000, 0.7, 0.05);
         {
             std::ostringstream oss;
             oss.setf(std::ios::fixed);
